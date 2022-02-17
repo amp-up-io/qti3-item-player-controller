aa855ed2e56d62b8029b9a0849f0c46de736f179
Paul Grudnitski <paul.grudnitski@pearson.com>
2022-02-17 09:08:30 -0800

    Add Sample for qti-text-entry-interaction

--- src/helpers/TestFactory.js
+++ src/helpers/TestFactory.js
@@ -34,8 +34,8 @@ const tests = [
   },
   {
     "id": "2",
-    "title": "L2 - I19b Shared CSS",
-    "description": "Demonstrates QTI3 Player support for all Level 2 conformance tests of Shared CSS.",
+    "title": "L2 - I19b Shared Layout and Styling CSS",
+    "description": "Demonstrates QTI3 Player support for all Level 2 conformance tests of Shared Layout and Styling CSS.",
     "count": "6",
     "items": [
       {
@@ -80,6 +80,174 @@ const tests = [
       },
       {
         "identifier": "i19b-shared-css-vocab-6",
+        "sessionControl": {
+          "showFeedback": true,
+          "validateResponses": false,
+          "submissionMode": "individual"
+        }
+      }
+    ]
+  },
+  {
+    "id": "3",
+    "title": "L1 - Q20 Text Entry Interaction",
+    "description": "Demonstrates QTI3 Player support for all Level 1 conformance tests of Q2- Text Entry Interaction.  Note: The <strong>last item</strong> demonstrates a composite item that includes all permutations of qti-text-entry-interaction qti-input-width shared vocabulary.",
+    "count": "20",
+    "items": [
+      {
+        "identifier": "q20-textentry",
+        "sessionControl": {
+          "showFeedback": true,
+          "validateResponses": false,
+          "submissionMode": "individual"
+        }
+      },
+      {
+        "identifier": "q20-textentry-sv-1",
+        "sessionControl": {
+          "showFeedback": true,
+          "validateResponses": false,
+          "submissionMode": "individual"
+        }
+      },
+      {
+        "identifier": "q20-textentry-sv-2a",
+        "sessionControl": {
+          "showFeedback": true,
+          "validateResponses": false,
+          "submissionMode": "individual"
+        }
+      },
+      {
+        "identifier": "q20-textentry-sv-2b",
+        "sessionControl": {
+          "showFeedback": true,
+          "validateResponses": false,
+          "submissionMode": "individual"
+        }
+      },
+      {
+        "identifier": "q20-textentry-sv-2c",
+        "sessionControl": {
+          "showFeedback": true,
+          "validateResponses": false,
+          "submissionMode": "individual"
+        }
+      },
+      {
+        "identifier": "q20-textentry-sv-2d",
+        "sessionControl": {
+          "showFeedback": true,
+          "validateResponses": false,
+          "submissionMode": "individual"
+        }
+      },
+      {
+        "identifier": "q20-textentry-sv-2j",
+        "sessionControl": {
+          "showFeedback": true,
+          "validateResponses": false,
+          "submissionMode": "individual"
+        }
+      },
+      {
+        "identifier": "q20-textentry-sv-2e",
+        "sessionControl": {
+          "showFeedback": true,
+          "validateResponses": false,
+          "submissionMode": "individual"
+        }
+      },
+      {
+        "identifier": "q20-textentry-sv-2f",
+        "sessionControl": {
+          "showFeedback": true,
+          "validateResponses": false,
+          "submissionMode": "individual"
+        }
+      },
+      {
+        "identifier": "q20-textentry-sv-2g",
+        "sessionControl": {
+          "showFeedback": true,
+          "validateResponses": false,
+          "submissionMode": "individual"
+        }
+      },
+      {
+        "identifier": "q20-textentry-sv-2h",
+        "sessionControl": {
+          "showFeedback": true,
+          "validateResponses": false,
+          "submissionMode": "individual"
+        }
+      },
+      {
+        "identifier": "q20-textentry-sv-2k",
+        "sessionControl": {
+          "showFeedback": true,
+          "validateResponses": false,
+          "submissionMode": "individual"
+        }
+      },
+      {
+        "identifier": "q20-textentry-sv-2l",
+        "sessionControl": {
+          "showFeedback": true,
+          "validateResponses": false,
+          "submissionMode": "individual"
+        }
+      },
+      {
+        "identifier": "q20-textentry-sv-2m",
+        "sessionControl": {
+          "showFeedback": true,
+          "validateResponses": false,
+          "submissionMode": "individual"
+        }
+      },
+      {
+        "identifier": "q20-textentry-sv-2n",
+        "sessionControl": {
+          "showFeedback": true,
+          "validateResponses": false,
+          "submissionMode": "individual"
+        }
+      },
+      {
+        "identifier": "q20-textentry-sv-2o",
+        "sessionControl": {
+          "showFeedback": true,
+          "validateResponses": false,
+          "submissionMode": "individual"
+        }
+      },
+      {
+        "identifier": "q20-textentry-sv-2p",
+        "sessionControl": {
+          "showFeedback": true,
+          "validateResponses": false,
+          "submissionMode": "individual"
+        }
+      },
+      {
+        "identifier": "q20-textentry-sv-2i",
+        "sessionControl": {
+          "showFeedback": true,
+          "validateResponses": false,
+          "submissionMode": "individual"
+        }
+      },
+      {
+        "identifier": "q20-textentry-sv-3",
+        "sessionControl": {
+          "showFeedback": true,
+          "validateResponses": false,
+          "submissionMode": "individual"
+        }
+      },
+      {
+        "identifier": "q20-textentry-composite",
         "sessionControl": {
           "showFeedback": true,
           "validateResponses": false,
