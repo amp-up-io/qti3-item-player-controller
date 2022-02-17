d32164d454d1c859e9c8529369a378374a371130
Paul Grudnitski <paul.grudnitski@pearson.com>
2022-02-17 09:08:10 -0800

    Support html in Start Panel

--- src/components/StartPanel.vue
+++ src/components/StartPanel.vue
@@ -3,7 +3,7 @@
     <div class="col-6 offset-3 shadow-sm p-5 bg-light bg-gradient border border-2 rounded-3">
       <h4 class="text-break">{{ title }}</h4>
       <p class="fs-4">{{maxItems}} Questions</p>
-      <p>{{ description }}</p>
+      <p v-html="description"></p>
       <button type="button" @click="handleStartClick" class="btn btn-primary btn-lg">Start</button>
     </div>
   </div>
