bb572d1b8f7d59d6be606d958a4b40a116f237a7
paulgrudnitski <paul.grudnitski@gmail.com>
2024-03-06 13:08:01 -0800

    Update icon image and title

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -3,7 +3,7 @@
     <div class="container-fluid">
       <div class="navbar-brand router-link">
         <router-link :to="{ name: 'Home', params: { } }">
-          <img src="@/assets/amp-up-io.svg" class="me-2" width="32" height="32" alt=""/>
+          <img src="@/assets/amp-up-io.svg" class="me-2" width="32" height="32" title="Amp-up.io" alt=""/>
           Test Runner
         </router-link>
       </div>
