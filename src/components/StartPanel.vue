ce0181353f879f8d3459db80088a703f4dde64c8
paulgrudnitski <paul.grudnitski@gmail.com>
2025-07-17 11:21:32 -0700

    Improve StartPanel header element

--- src/components/StartPanel.vue
+++ src/components/StartPanel.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="card">
     <div class="card-body">
-      <h4 class="text-break">{{ title }}</h4>
-      <p class="fs-4">{{maxItems}} Questions</p>
+      <h2 class="fs-4 text-break">{{ title }}</h2>
+      <p class="fs-5">{{maxItems}} Questions</p>
       <p v-html="description"></p>
       <button type="button" @click="handleStartClick" class="btn btn-primary btn-lg">Start</button>
     </div>
